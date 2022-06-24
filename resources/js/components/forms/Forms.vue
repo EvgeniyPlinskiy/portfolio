e453042d6de2ad2af79a1c948508ece571231310
EvgeniyPlinskiy <gkolbasoff@gmail.com>
2022-06-24 19:35:13 +0300

    checkbox

--- resources/js/components/forms/Forms.vue
+++ resources/js/components/forms/Forms.vue
@@ -1,15 +1,18 @@
 <template>
   <PersonalCard />
+  <CheckBox />
 </template>
 
 
 <script>
 import PersonalCard from './components/PersonalCard.vue'
+import CheckBox from './components/CheckBox.vue'
 
 
 export default {
   components: {
-    PersonalCard
+    PersonalCard,
+    CheckBox
   },
 };
 </script>
---
+++ resources/js/components/forms/components/CheckBox.vue
@@ -0,0 +1,10 @@
+<template>
+<div class="info-box-title">
+    <h3>Check Box</h3>
+  </div>
+<div class="check-box">
+    <input type="checkbox">
+</div>
+</template>
+<script>
+</script>
